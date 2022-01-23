acaf0b558e831b88e722ca295ded24b9551bcaeb
TK303 <34978324+whatis-tk303@users.noreply.github.com>
2022-01-23 17:35:57 +0900

    プロジェクトの名前を変更

--- main.ts
+++ main.ts
@@ -6,13 +6,13 @@ function initMotor (数値: number) {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 数値)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 数値)
 }
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    speed = 77
-    initMotor(speed)
-})
 input.onButtonPressed(Button.B, function () {
     speed += -5
     initMotor(Math.constrain(speed, 0, 180))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    speed = 77
+    initMotor(speed)
 })
 let speed = 0
 speed = 77
