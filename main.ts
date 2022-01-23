59dabe4ceb7f81264070e68777c70c23965178df
TK303 <34978324+whatis-tk303@users.noreply.github.com>
2022-01-23 15:24:57 +0900

    モーターを初期化する関数を追加した

--- main.ts
+++ main.ts
@@ -1,22 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     speed += 5
-    initMotor(Math.constrain(speed, 0, 180))
+    setMotor(Math.constrain(speed, 0, 180))
 })
-function initMotor (数値: number) {
+function initMotor () {
+    speed = 77
+    setMotor(speed)
+}
+input.onButtonPressed(Button.B, function () {
+    speed += -5
+    setMotor(Math.constrain(speed, 0, 180))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    initMotor()
+})
+function setMotor (数値: number) {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 数値)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 数値)
 }
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    speed = 77
-    initMotor(speed)
-})
-input.onButtonPressed(Button.B, function () {
-    speed += -5
-    initMotor(Math.constrain(speed, 0, 180))
-})
 let speed = 0
-speed = 77
-initMotor(speed)
+initMotor()
 basic.forever(function () {
     basic.showNumber(speed)
 })
